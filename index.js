9a341d5433d6ba5a271bddb979f69c7f97e778dd
Mvrcosi <saucedoimarcos@gmail.com>
2022-02-16 17:06:09 -0800

    connected mongo database

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+
 const app = express()
+
+dotenv.config()
+const PORT = process.env.PORT || 5000;
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname))
+
+
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to DB')
+    }).catch((err) => {
+        console.log(err.messsage)
+    })
+
 
 
 
@@ -9,6 +32,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('serving on port 3000')
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
